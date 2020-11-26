b5c7feb88dc221c23e9060b9b48713f3d2106c10
samir <1803770129@qq.com>
2020-11-26 14:26:08 +0800

    第一次提交

---
+++ components/like/index.js
@@ -0,0 +1,37 @@
+// components/like/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    count:{
+      type:Number,
+      value:99
+    },
+    like:{
+      type:Boolean,
+      value:false
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    yes_url: 'images/like.png',
+    no_url: 'images/like@dis.png',
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onLike:function(e){
+      let count = this.properties.like? this.properties.count - 1 : this.properties.count + 1
+      this.setData({
+        like:!this.properties.like,
+        count:count
+      })
+    }
+  }
+})
