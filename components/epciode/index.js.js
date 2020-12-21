d1125b34851beffbd8b60e5172344b42d39c0822
samir <1803770129@qq.com>
2020-12-21 17:35:30 +0800

    comiit

---
+++ components/epciode/index.js.js
@@ -0,0 +1,24 @@
+// components/epciode/index.js.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    index:Number
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    year:null,
+    month:null
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    
+  }
+})
